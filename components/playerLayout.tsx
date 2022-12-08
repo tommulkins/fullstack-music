4b9e67ce08971453c6662cf930d11cb309baf384
Tom Mulkins <tom.mulkins@gmail.com>
2022-12-08 16:48:45 -0500

    sidebar

---
+++ components/playerLayout.tsx
@@ -0,0 +1,28 @@
+import { Box } from "@chakra-ui/layout";
+import SideBar from "./sidebar";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      width="100vw"
+      height="100vh"
+      bg="gray.100"
+    >
+      <Box position="absolute" top="0" width="250px" left="0">
+        <SideBar />
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        player
+      </Box>
+    </Box>
+  );
+};
+
+export default PlayerLayout;
